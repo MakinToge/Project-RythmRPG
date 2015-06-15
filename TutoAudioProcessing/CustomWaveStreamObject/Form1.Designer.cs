6dc0696ec62ae96a26e8cf6fe064e15bee638dd9
gyveni <ronan.yven@isen-lille.fr>
2015-06-15 17:20:30 +0200

    CustomWaveStream

---
+++ TutoAudioProcessing/CustomWaveStreamObject/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace CustomWaveStreamObject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startTone = new System.Windows.Forms.Button();
+            this.stopTone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startTone
+            // 
+            this.startTone.Location = new System.Drawing.Point(50, 58);
+            this.startTone.Name = "startTone";
+            this.startTone.Size = new System.Drawing.Size(191, 23);
+            this.startTone.TabIndex = 0;
+            this.startTone.Text = "Start Tone";
+            this.startTone.UseVisualStyleBackColor = true;
+            this.startTone.Click += new System.EventHandler(this.startTone_Click);
+            // 
+            // stopTone
+            // 
+            this.stopTone.Location = new System.Drawing.Point(50, 109);
+            this.stopTone.Name = "stopTone";
+            this.stopTone.Size = new System.Drawing.Size(191, 23);
+            this.stopTone.TabIndex = 1;
+            this.stopTone.Text = "Stop Tone";
+            this.stopTone.UseVisualStyleBackColor = true;
+            this.stopTone.Click += new System.EventHandler(this.stopTone_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 266);
+            this.Controls.Add(this.stopTone);
+            this.Controls.Add(this.startTone);
+            this.Name = "Form1";
+            this.Text = "WaveStream";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startTone;
+        private System.Windows.Forms.Button stopTone;
+    }
+}
+
---
+++ TutoAudioProcessing/CustomWaveStreamObject/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NAudio.Wave;
+
+namespace CustomWaveStreamObject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private DirectSoundOut output = null;
+        private BlockAlignReductionStream stream = null;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startTone_Click(object sender, EventArgs e)
+        {
+            WaveTone tone = new WaveTone(3000, 0.1); // 1000 Hz and low amplitude (between 1 and -1)
+            stream = new BlockAlignReductionStream(tone);
+
+            output = new DirectSoundOut();
+            output.Init(stream);
+            output.Play();
+        }
+
+        private void stopTone_Click(object sender, EventArgs e)
+        {
+            if (output != null) output.Stop();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (output != null)
+            {
+                output.Dispose();
+                output = null;
+            }
+            if (stream != null)
+            {
+                stream.Dispose();
+                stream = null;
+            }
+        }
+
+        public class WaveTone : WaveStream
+        {
+            private double frequency;
+            private double amplitude;
+            private double time;
+
+            public WaveTone(double f, double a)
+            {
+                this.time = 0;
+                this.frequency = f;
+                this.amplitude = a;
+            }
+
+            //must be override
+            public override long Position
+            {
+                get;
+                set;
+            }
+
+            //must be override
+            public override long Length
+            {
+                get { return long.MaxValue; }
+            }
+
+            //must be override
+            public override WaveFormat WaveFormat
+            {
+                get { return new WaveFormat(44100, 16, 1); } // (samples per sec , bits per sample, number of channels)
+            }
+
+            //must be override
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                int samples = count / 2; // divide by 2 (bytes) because we have 16 bits per sec
+                for (int i = 0; i < samples; i++)
+                {
+                    double sine = amplitude * Math.Sin(Math.PI * 2 * frequency * time);
+                    time += 1.0 / 44100;
+                    short truncated = (short)Math.Round(sine * (Math.Pow(2, 15) - 1)); // - 1 because signed value, so - 1 bit
+                    buffer[i * 2] = (byte)(truncated & 0x00ff); //first byte of the short value
+                    buffer[i * 2 + 1] = (byte)((truncated & 0xff00) >> 8); //second byte of the short value
+                }
+
+                return count; //number of bytes to read
+            }
+        }
+    }
+}
