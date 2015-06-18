3fe486a557d8c967a8d6a9859de30d7760443362
Floriel Fedry <floriel.fedry@isen-lille.fr>
2015-06-18 15:15:35 +0200

    StartMenu Select Save

--- RythmRPG/RythmRPG/RythmRPG/Pages/StartMenu.cs
+++ RythmRPG/RythmRPG/RythmRPG/Pages/StartMenu.cs
@@ -10,21 +10,33 @@ namespace RythmRPG.Pages {
         public Sprite MainImage { get; set; }
         public Sprite Start { get; set; }
         public Sprite Options { get; set; }
-        public Sprite Achievements { get; set; }
+        public Sprite[] SaveSprites { get; set; }
         public Sprite Scores { get; set; }
+        public Sprite LeftSave { get; set; }
+        public Sprite RightSave { get; set; }
 
         public override void Initialize() {
             this.MainImage = new Sprite(0, 0, Game1.Width, Game1.Height);
             this.Start = new Sprite(3 * Game1.UnitX, 4 * Game1.UnitY, Game1.ButtonWidth, 2 * Game1.ButtonHeight);
             this.Options = new Sprite(3 * Game1.UnitX, 8 * Game1.UnitY, Game1.ButtonWidth, 2* Game1.ButtonHeight);
-            this.Achievements = new Sprite(3 * Game1.UnitX, 13 * Game1.UnitY, Game1.ButtonWidth, Game1.ButtonHeight);
+            this.SaveSprites = new Sprite[Save.NB_SAVE];
+            for (int i = 0; i < Save.NB_SAVE; i++) {
+                this.SaveSprites[i] = new Sprite(5 * Game1.UnitX, 13 * Game1.UnitY, 4 * Game1.UnitX, Game1.UnitY);
+            }
+            this.LeftSave = new Sprite(3 * Game1.UnitX, 13 * Game1.UnitY, Game1.UnitX, Game1.UnitY);
+            this.RightSave = new Sprite(10 * Game1.UnitX, 13 * Game1.UnitY, Game1.UnitX, Game1.UnitY);
+            
             this.Scores = new Sprite(3 * Game1.UnitX, 15 * Game1.UnitY, Game1.ButtonWidth, Game1.ButtonHeight);
         }
         public override void LoadContent(Microsoft.Xna.Framework.Content.ContentManager content) {
             this.MainImage.LoadContent(content, "StartMenu/StartMenu");
             this.Start.LoadContent(content, "StartMenu/Start");
             this.Options.LoadContent(content, "StartMenu/Options");
-            this.Achievements.LoadContent(content, "StartMenu/Achievements");
+            this.LeftSave.LoadContent(content, "Options/ArrowLeft");
+            this.RightSave.LoadContent(content, "Options/ArrowRight");
+            for (int i = 0; i < Save.NB_SAVE; i++) {
+                this.SaveSprites[i].LoadContent(content, "StartMenu/Save" + (i+1));
+            }
             this.Scores.LoadContent(content, "StartMenu/Scores");
         }
         public override void HandleInput(Microsoft.Xna.Framework.Input.KeyboardState previousKeyboardState, Microsoft.Xna.Framework.Input.KeyboardState currentKeyboardState, Microsoft.Xna.Framework.Input.MouseState previousMouseState, Microsoft.Xna.Framework.Input.MouseState currentMouseState) {
@@ -40,6 +52,12 @@ namespace RythmRPG.Pages {
                 else if (isOver(mouse, Start)) {
                     Game1.GameState = GameState.GameMenu;
                 }
+                else if (isOver(mouse, LeftSave) && Game1.Save.SelectedSave > 0) {
+                    Game1.Save.SelectedSave -= 1;
+                }
+                else if (isOver(mouse, RightSave) && Game1.Save.SelectedSave < Save.NB_SAVE - 1) {
+                    Game1.Save.SelectedSave += 1;
+                }
             }
         }
 
@@ -47,7 +65,9 @@ namespace RythmRPG.Pages {
             this.MainImage.Draw(spriteBatch, gameTime);
             this.Start.Draw(spriteBatch, gameTime);
             this.Options.Draw(spriteBatch, gameTime);
-            this.Achievements.Draw(spriteBatch, gameTime);
+            this.LeftSave.Draw(spriteBatch, gameTime);
+            this.RightSave.Draw(spriteBatch, gameTime);
+            this.SaveSprites[Game1.Save.SelectedSave].Draw(spriteBatch, gameTime);
             this.Scores.Draw(spriteBatch, gameTime);
         }
     }
