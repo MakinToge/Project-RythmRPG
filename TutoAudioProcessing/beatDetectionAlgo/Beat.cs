76968a607d7d0d9af00a7d7eeb9c98dbbc82e412
Adrien Veroone <adrien.veroone@isen-lille.fr>
2015-06-18 12:48:08 +0200

    beat detection algo v1

---
+++ TutoAudioProcessing/beatDetectionAlgo/Beat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beatDetectionAlgo
+{
+    class Beat
+    {
+        public double Time { get; set; }
+        public int Subband { get; set; }
+
+        public Beat(double time, int subband)
+        {
+            this.Time = time;
+            this.Subband = subband;
+        }
+        public override string ToString()
+        {
+            return string.Format("beat at {0} in subb {1}", this.Time, this.Subband);
+        }
+    }
+}
