cf164795734ffc65d3365544834a746f0fb8c65c
gyveni <ronan.yven@isen-lille.fr>
2015-06-21 18:09:31 +0200

    loop music

--- RythmRPG/RythmRPG/RythmRPG/Pages/Pause.cs
+++ RythmRPG/RythmRPG/RythmRPG/Pages/Pause.cs
@@ -33,6 +33,8 @@ namespace RythmRPG.Pages {
                 }
                 else if (isOver(mouse, GameMenu)) {// Clique sur GameMenu
                     StartMenu.EffectClick.Play();
+                    StartMenu.MainTheme.IsLooped = true;
+                    StartMenu.MainTheme.Volume = StartMenu.VOLUME_ON;
                     StartMenu.MainTheme.Play();
                     Game1.GameState = GameState.GameMenu;
                 }
