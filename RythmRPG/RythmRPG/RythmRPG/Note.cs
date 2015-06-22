4c7c85edba4ef7b79b242ab5f587b0828a912f14
Adrien Veroone <adrien.veroone@isen-lille.fr>
2015-06-22 05:31:21 +0200

    added missing file

---
+++ RythmRPG/RythmRPG/RythmRPG/Note.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using RythmRPG.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RythmRPG
+{
+    class Note : Sprite
+    {
+        public double Time { get; private set; }
+        public int Line { get; set; }
+
+        public Note(double time, int line):base()
+        {
+            
+            this.Time = time;
+            this.Line = line;
+
+            this.Direction = new Vector2(1, 0);//x = 1 (left)
+            this.Position = new Vector2(2 * Game1.UnitX, (12 + line) * Game1.UnitY);
+            this.Size = new Vector2(Game1.UnitX,Game1.UnitY);
+            this.Speed = 0.3f + 0.1f * (float)Game1.Difficulty;
+
+            //Load png
+            string assetName = "MusicPlaying/note";
+            if (line == 0)
+            {
+                assetName = "MusicPlaying/noteGreen";
+            }
+            else if (line == 1)
+            {
+                assetName = "MusicPlaying/noteRed";
+            }
+            else if (line == 2)
+            {
+                assetName = "MusicPlaying/noteYellow";
+            }
+            else if (line == 3)
+            {
+                assetName = "MusicPlaying/noteBlue";
+            }
+            else
+            {
+                assetName = "MusicPlaying/noteOrange";
+            }
+            this.LoadContent(MusicPlaying.Content, assetName);
+        }
+        public override string ToString()
+        {
+            return string.Format("beat at {0} on line {1}", this.Time, this.Line);
+        }
+    }
+}
